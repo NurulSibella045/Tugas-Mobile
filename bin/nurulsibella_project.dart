1f032d930d66952cce4be83b5105ee3b9d563381
Nurul Sibella <nurulsibellave95@gmail.com>
2025-08-31 12:30:35 +0900

    komit pertama

---
+++ bin/nurulsibella_project.dart
@@ -0,0 +1,5 @@
+import 'package:nurulsibella_project/nurulsibella_project.dart' as nurulsibella_project;
+
+void main(List<String> arguments) {
+  print('Hello world: ${nurulsibella_project.calculate()}!');
+}
---
+++ test/nurulsibella_project_test.dart
@@ -0,0 +1,8 @@
+import 'package:nurulsibella_project/nurulsibella_project.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
